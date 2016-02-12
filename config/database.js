1272ad6f5b2e6e5318ccfbdec92bb7393b3559dd
David Sypien <davidsypien@gmail.com>
2016-02-11 21:50:05 -0500

    simplify the api scaffold

--- config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	url: "mongodb://AspireService:YeAhBo11111a5pir333@ds041140.mongolab.com:41140/aspire"
-};
--- server.js
+++ server.js
@@ -1,73 +1,28 @@
 var express = require('express');
 var app = express();
-
-var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash = require('connect-flash');
 var morgan = require('morgan');
-var session = require('express-session');
+var mongoose = require('mongoose');
 
-var configDB = require('./config/database.js');
+var jwt = require('jsonwebtoken');
+var config = require('./config');
 
-// configuration
-mongoose.connect(configDB.url);
-//require('./config/passport')(passport);
+var indexRoute = require('./routes/index');
+var registerRoute = require('./routes/register');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Config
+var port = process.env.PORT || 3003;
+mongoose.connect(config.database);
+app.set('superSecret', config.secret);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Passport setup
-app.use(session({ secret: 'z7Xd2Wb%3@cM90wp*dX@98Po@1^dMn14n2'})); // session secret
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
+app.use(morgan('dev'));
 
-require('./app/routes.js')(app, passport);
+//routes
+app.use('/', indexRoute);
+app.use('/register', registerRoute);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+app.listen(port);
+console.log('Server running on port ' + port);
