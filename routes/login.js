7d4d92ede7b0dd18ebc7e5294f6c298673eec7c8
David Sypien <davidsypien@gmail.com>
2016-02-13 08:50:55 -0500

    implement login route

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,27 @@ var router 		= express.Router();
 var User 		= require('../app/models/user');
 
 router.post('/', function(req, res){
+	var email = req.body.email;
+	var pwd = req.body.password;
 
+	User.findOne({'local.email' : email}, function(err, user){
+		if(err){
+			res.json({success : false, message: err.message});
+			return;
+		}
+
+		if(!user){
+			res.json({success : false, message: "A user with this email does not exist."});
+			return;
+		}
+
+		if(user.validPassword(pwd)){
+			res.json({success: true});
+		}
+		else{
+			res.json({success : false, message: "The email and password you entered do not match."});
+		}
+	});
 });
 
 module.exports = router;
